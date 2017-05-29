309ff7927a3e397a2ff81b681e535fa0b274b7e7
jonnrauber <natanrauber97@gmail.com>
2017-05-29 19:02:11 -0300

    alguns comentários iniciais e modularização

--- afd.py
+++ afd.py
@@ -1,12 +1,35 @@
-def main():
-	transicoes = {}
-	transicoes['S'] = {}
-	simbolos = []
-	with open("entrada.txt", "r") as arquivo:
-		
-		
+# dicionário chave-valor que mapeia estados, caracteres e transições, 
+# na forma
+# AFD = {
+#		 'S': {
+#			   's': ['A']
+#			  }, 
+#		 'A': {
+#			   'e': ['B']
+#			  }
+#		}
 
-#abrir arquivo para leitura
+transicoes = {} 
+transicoes['S'] = {}
+simbolos = []
+
+def leToken(linha):
+	global transicoes, simbolos #tem que escrever global quando vc quer alterar os valores das variaveis globais
+
+def leGR(linha):
+	global transicoes, simbolos
+
+
+def main():
+	#abre o arquivo em modo de leitura
+	with open("entrada.txt", "r") as arquivo:
+		for linha in arquivo:
+			#não precisa nem ter cuidado se é EOF, objetos do tipo FILE são iteráveis em python
+			if(linha[0] != '<') 
+				leToken(linha)
+			else
+				leGR(linha)
+
 
 
 
