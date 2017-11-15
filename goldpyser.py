0eb5697d47d977ef4034616d4cb1e2526cf0ce12
Gabriel Henrique Rudey <ucraaa@gmail.com>
2017-11-15 16:14:06 -0200

    Adiciona modulo de interpretação do XML do GoldParser
    O módulo `goldpyser` faz a leitura do XML do GoldParser, mais
    especificamente da seção da tabela LALR, para gerar a tabela LALR
    no formato requerido pelo projeto [este].
    
    Uso:
    ```python
    import goldpyser
    
    goldpyser.read_from_xml('./gramatica.xml')
    ```

---
+++ goldpyser.py
@@ -0,0 +1,66 @@
+from xml.etree import ElementTree
+from html      import unescape
+from simbolo   import simbolo
+from enum      import Enum
+
+class ActionType(Enum):
+    """
+    In GoldParser XML, each LALRAction has an attribute "Action" whose means the Transference,
+    Reduction, Jump or Acceptance, for convenience and redability this enum was created to map
+    each one.
+    """
+    Transference = 1
+    Reduction    = 2
+    Jump         = 3
+    Accept       = 4
+
+
+# Text representation of ActionType
+ACTIONTEXT = {                     
+    ActionType.Transference: 'T',
+    ActionType.Reduction:    'R',
+    ActionType.Jump:         '',
+    ActionType.Accept:       'AC'
+}
+
+
+def read_from_xml(path):
+    """
+    Read a Gold-Parser XML dump and creates the LALR Parsing Table using a list of `simbolo`
+    """
+    tree = ElementTree.parse(path)
+    root = tree.getroot()
+    LALRTable = []
+
+    symbols = root.find('m_Symbol')
+
+    for symbol in symbols.findall('Symbol'):
+        sym = simbolo()
+        # Sometimes in XML the symbols are escaped in HTML format. E.g.: &lt; = <, then we need to
+        # undo this convertion
+        sym.rotulo = unescape(symbol.attrib['Name'])
+        LALRTable.append(sym)
+
+    table_node = root.find('LALRTable')
+
+    for state in table_node.findall('LALRState'):
+        state_number = int(state.attrib['Index'])
+
+        for action in state.findall('LALRAction'):
+            symbol_index  = int(action.attrib['SymbolIndex'])
+            action_number = int(action.attrib['Action'])
+            goto          = action.attrib['Value']
+
+            # Here in parse_action, the attribute 'Action' in XML is 1, 2, 3 or 4. Each one
+            # represents an action decribed in ActionType enum.
+            parse_action = ActionType(action_number)
+
+            if parse_action == ActionType.Accept:
+                transition_str = ACTIONTEXT[parse_action]
+            else:
+                transition_str = "%s%s" % (ACTIONTEXT[parse_action], goto)
+
+            LALRTable[symbol_index].transicoes.insert(state_number, transition_str)
+
+    return LALRTable
+
